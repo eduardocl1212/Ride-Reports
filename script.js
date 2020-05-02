826f36fb904e01fa3a098d0a623ca11ed90cf669
Eduardo Cabrera Lopez <eduardocl@outlook.com>
2020-05-02 14:10:59 -0500

    Clock

--- script.js
+++ script.js
@@ -1,207 +1,29 @@
-// small circle radius
-const r1 = 5;
-const r2 = 10;
-const r3 = 15;
-const width = window.innerWidth;
-const height = window.innerHeight;
+// START CLOCK SCRIPT
 
-const minWH = Math.min(width, height);
+Number.prototype.pad = function(n) {
+  for (var r = this.toString(); r.length < n; r = 0 + r);
+  return r;
+};
 
-let maxSize;
-if (minWH < 430) {
-  maxSize = minWH - 30;
-} else {
-  maxSize = 400;
+function updateClock() {
+  var now = new Date();
+  var milli = now.getMilliseconds(),
+    sec = now.getSeconds(),
+    min = now.getMinutes(),
+    hou = now.getHours(),
+    mo = now.getMonth(),
+    dy = now.getDate(),
+    yr = now.getFullYear();
+  var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  var tags = ["mon", "d", "y", "h", "m", "s", "mi"],
+    corr = [months[mo], dy, yr, hou.pad(2), min.pad(2), sec.pad(2), milli];
+  for (var i = 0; i < tags.length; i++)
+    document.getElementById(tags[i]).firstChild.nodeValue = corr[i];
 }
 
-// utils
-// deg to radian
-const rad = a => Math.PI * (a - 90) / 180;
+function initClock() {
+  updateClock();
+  window.setInterval("updateClock()", 1);
+}
 
-// relative polar coordinates
-const rx = (r, a, c) => c + r * Math.cos(rad(a, c));
-
-const ry = (r, a, c) => c + r * Math.sin(rad(a));
-
-// get hours, minutes, and seconds
-const HMS = t => ({
-  h: t.getHours(),
-  m: t.getMinutes(),
-  s: t.getSeconds() });
-
-
-const pathStringVars = (c, r, time) => {
-  // center, radius and time = this.props		
-  // const {c,r,time} = p
-  const { h, m, s } = HMS(time);
-
-  // divide 360 deg by 12hrs, 60min, and 60s 
-  const hAngFact = 30;
-  const mAngFact = 6;
-  const sAngFact = 6;
-
-  // calc relative coordinates 		
-  const hx = rx(r - 30, hAngFact * h, c);
-  const hy = ry(r - 30, hAngFact * h, c);
-  const mx = rx(r - 30, mAngFact * m, c);
-  const my = ry(r - 30, mAngFact * m, c);
-  const sx = rx(r - 30, sAngFact * s, c);
-  const sy = ry(r - 30, sAngFact * s, c);
-
-  return { hx, hy, mx, my, sx, sy };
-};
-
-const TextTime = ({ x, y, time }) => {
-  const str = time.toTimeString().slice(0, 8).replace(/:/g, " : ");
-  return (
-    React.createElement("text", { x: x, y: y, className: "textTime" },
-    str));
-
-
-};
-
-// Circle component
-const Circle = ({ cx, cy, r, cl }) =>
-React.createElement("circle", {
-  cx: cx,
-  cy: cy,
-  r: r,
-  className: cl });
-
-
-
-// Single spike
-const Spike = ({ x1, x2, y1, y2 }) =>
-React.createElement("line", {
-  className: "spike",
-  x1: x1,
-  x2: x2,
-  y1: y1,
-  y2: y2 });
-
-
-
-const spikeNodes = (c, r) => {
-  const increment = 30;
-  const nodes = [];
-
-  for (let i = 1; i < 13; i++) {
-    let ang = i * increment;
-
-    let temp =
-    React.createElement(Spike, {
-      x1: rx(r - 5, ang, c),
-      x2: rx(r - 10, ang, c),
-      y1: ry(r - 5, ang, c),
-      y2: ry(r - 10, ang, c),
-      key: i });
-
-
-    nodes.push(temp);
-  }
-  return nodes;
-};
-
-// populate Spikes
-const Spikes = ({ c, r }) =>
-React.createElement("g", null,
-spikeNodes(c, r));
-
-
-
-// triangle component
-const Triangle = ({ c, r, time }) => {
-  const { hx, hy, mx, my, sx, sy } = pathStringVars(c, r, time);
-  const path = `M${hx},${hy} L${mx},${my} L${sx},${sy} L${hx},${hy}`;
-  return (
-    React.createElement("path", {
-      className: "triangle",
-      d: path }));
-
-
-};
-
-// Secondary circles
-const SecCircle = ({ c, r, time }) => {
-  const { hx, hy, mx, my, sx, sy } = pathStringVars(c, r, time);
-  return (
-    React.createElement("g", null,
-    React.createElement(Circle, { cl: "secCircle", cx: hx, cy: hy, r: r3 }),
-    React.createElement(Circle, { cl: "secCircle", cx: mx, cy: my, r: r2 }),
-    React.createElement(Circle, { cl: "secCircle", cx: sx, cy: sy, r: r1 })));
-
-
-};
-
-// main container
-class Clock extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      time: new Date() };
-
-  }
-
-  componentDidMount() {
-    window.setInterval(() => {
-      this.setState({
-        time: new Date() });
-
-    }, 1000);
-  }
-
-  render() {
-
-    const size = maxSize;
-
-    const viewBox = `0 0 ${size} ${size}`;
-
-    const mid = size / 2;
-
-    const paddedRadius = (size - 30) / 2;
-
-    return (
-      React.createElement("svg", { xmlns: "http://www.w3.org/svg/2000",
-        viewBox: viewBox,
-        width: size,
-        height: size },
-      React.createElement(Circle, {
-        cl: "outerRing",
-        cx: mid,
-        cy: mid,
-        r: mid - 5 }),
-
-      React.createElement(Circle, {
-        cl: "primCircle",
-        cx: mid,
-        cy: mid,
-        r: mid - 15 }),
-
-      React.createElement(Spikes, {
-        c: mid,
-        r: paddedRadius }),
-
-      React.createElement(Triangle, {
-        c: mid,
-        r: paddedRadius,
-        time: this.state.time }),
-
-      React.createElement(SecCircle, {
-        c: mid,
-        r: paddedRadius,
-        time: this.state.time }),
-
-      React.createElement(TextTime, {
-        time: this.state.time,
-        x: mid,
-        y: mid })));
-
-
-
-  }}
-
-
-ReactDOM.render(
-React.createElement(Clock, null),
-document.querySelector('.clock'));
+// END CLOCK SCRIPT
