dd5e7d34dc70c4e176523ebb1b1378a90e6eef97
Eduardo Cabrera Lopez <eduardocl@outlook.com>
2020-05-02 13:55:51 -0500

    NewClock

--- script.js
+++ script.js
@@ -1,81 +1,207 @@
-class TextScramble {
-  constructor(el) {
-    this.el = el;
-    this.chars = '!<>-_\\/[]{}—=+*^?#________';
-    this.update = this.update.bind(this);
+// small circle radius
+const r1 = 5;
+const r2 = 10;
+const r3 = 15;
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const minWH = Math.min(width, height);
+
+let maxSize;
+if (minWH < 430) {
+  maxSize = minWH - 30;
+} else {
+  maxSize = 400;
+}
+
+// utils
+// deg to radian
+const rad = a => Math.PI * (a - 90) / 180;
+
+// relative polar coordinates
+const rx = (r, a, c) => c + r * Math.cos(rad(a, c));
+
+const ry = (r, a, c) => c + r * Math.sin(rad(a));
+
+// get hours, minutes, and seconds
+const HMS = t => ({
+  h: t.getHours(),
+  m: t.getMinutes(),
+  s: t.getSeconds() });
+
+
+const pathStringVars = (c, r, time) => {
+  // center, radius and time = this.props		
+  // const {c,r,time} = p
+  const { h, m, s } = HMS(time);
+
+  // divide 360 deg by 12hrs, 60min, and 60s 
+  const hAngFact = 30;
+  const mAngFact = 6;
+  const sAngFact = 6;
+
+  // calc relative coordinates 		
+  const hx = rx(r - 30, hAngFact * h, c);
+  const hy = ry(r - 30, hAngFact * h, c);
+  const mx = rx(r - 30, mAngFact * m, c);
+  const my = ry(r - 30, mAngFact * m, c);
+  const sx = rx(r - 30, sAngFact * s, c);
+  const sy = ry(r - 30, sAngFact * s, c);
+
+  return { hx, hy, mx, my, sx, sy };
+};
+
+const TextTime = ({ x, y, time }) => {
+  const str = time.toTimeString().slice(0, 8).replace(/:/g, " : ");
+  return (
+    React.createElement("text", { x: x, y: y, className: "textTime" },
+    str));
+
+
+};
+
+// Circle component
+const Circle = ({ cx, cy, r, cl }) =>
+React.createElement("circle", {
+  cx: cx,
+  cy: cy,
+  r: r,
+  className: cl });
+
+
+
+// Single spike
+const Spike = ({ x1, x2, y1, y2 }) =>
+React.createElement("line", {
+  className: "spike",
+  x1: x1,
+  x2: x2,
+  y1: y1,
+  y2: y2 });
+
+
+
+const spikeNodes = (c, r) => {
+  const increment = 30;
+  const nodes = [];
+
+  for (let i = 1; i < 13; i++) {
+    let ang = i * increment;
+
+    let temp =
+    React.createElement(Spike, {
+      x1: rx(r - 5, ang, c),
+      x2: rx(r - 10, ang, c),
+      y1: ry(r - 5, ang, c),
+      y2: ry(r - 10, ang, c),
+      key: i });
+
+
+    nodes.push(temp);
   }
-  setText(newText) {
-    const oldText = this.el.innerText;
-    const length = Math.max(oldText.length, newText.length);
-    const promise = new Promise(resolve => this.resolve = resolve);
-    this.queue = [];
-    for (let i = 0; i < length; i++) {
-      const from = oldText[i] || '';
-      const to = newText[i] || '';
-      const start = Math.floor(Math.random() * 40);
-      const end = start + Math.floor(Math.random() * 40);
-      this.queue.push({ from, to, start, end });
-    }
-    cancelAnimationFrame(this.frameRequest);
-    this.frame = 0;
-    this.update();
-    return promise;
+  return nodes;
+};
+
+// populate Spikes
+const Spikes = ({ c, r }) =>
+React.createElement("g", null,
+spikeNodes(c, r));
+
+
+
+// triangle component
+const Triangle = ({ c, r, time }) => {
+  const { hx, hy, mx, my, sx, sy } = pathStringVars(c, r, time);
+  const path = `M${hx},${hy} L${mx},${my} L${sx},${sy} L${hx},${hy}`;
+  return (
+    React.createElement("path", {
+      className: "triangle",
+      d: path }));
+
+
+};
+
+// Secondary circles
+const SecCircle = ({ c, r, time }) => {
+  const { hx, hy, mx, my, sx, sy } = pathStringVars(c, r, time);
+  return (
+    React.createElement("g", null,
+    React.createElement(Circle, { cl: "secCircle", cx: hx, cy: hy, r: r3 }),
+    React.createElement(Circle, { cl: "secCircle", cx: mx, cy: my, r: r2 }),
+    React.createElement(Circle, { cl: "secCircle", cx: sx, cy: sy, r: r1 })));
+
+
+};
+
+// main container
+class Clock extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      time: new Date() };
+
   }
-  update() {
-    let output = '';
-    let complete = 0;
-    for (let i = 0, n = this.queue.length; i < n; i++) {
-      let { from, to, start, end, char } = this.queue[i];
-      if (this.frame >= end) {
-        complete++;
-        output += to;
-      } else if (this.frame >= start) {
-        if (!char || Math.random() < 0.28) {
-          char = this.randomChar();
-          this.queue[i].char = char;
-        }
-        output += `<span class="dud">${char}</span>`;
-      } else {
-        output += from;
-      }
-    }
-    this.el.innerHTML = output;
-    if (complete === this.queue.length) {
-      this.resolve();
-    } else {
-      this.frameRequest = requestAnimationFrame(this.update);
-      this.frame++;
-    }
+
+  componentDidMount() {
+    window.setInterval(() => {
+      this.setState({
+        time: new Date() });
+
+    }, 1000);
   }
-  randomChar() {
-    return this.chars[Math.floor(Math.random() * this.chars.length)];
+
+  render() {
+
+    const size = maxSize;
+
+    const viewBox = `0 0 ${size} ${size}`;
+
+    const mid = size / 2;
+
+    const paddedRadius = (size - 30) / 2;
+
+    return (
+      React.createElement("svg", { xmlns: "http://www.w3.org/svg/2000",
+        viewBox: viewBox,
+        width: size,
+        height: size },
+      React.createElement(Circle, {
+        cl: "outerRing",
+        cx: mid,
+        cy: mid,
+        r: mid - 5 }),
+
+      React.createElement(Circle, {
+        cl: "primCircle",
+        cx: mid,
+        cy: mid,
+        r: mid - 15 }),
+
+      React.createElement(Spikes, {
+        c: mid,
+        r: paddedRadius }),
+
+      React.createElement(Triangle, {
+        c: mid,
+        r: paddedRadius,
+        time: this.state.time }),
+
+      React.createElement(SecCircle, {
+        c: mid,
+        r: paddedRadius,
+        time: this.state.time }),
+
+      React.createElement(TextTime, {
+        time: this.state.time,
+        x: mid,
+        y: mid })));
+
+
+
   }}
 
 
-// ——————————————————————————————————————————————————
-// Example
-// ——————————————————————————————————————————————————
-
-const phrases = [
-'QUEDATE EN CASA!',
-'GRUPO RIDE!',
-'VIAJA SEGURO',
-'VIAJA FACIL',
-'VIAJA RIDE!',
-'MUY PRONTO',
-'ESPERANOS',
-'CUIDATE'];
-
-
-const el = document.querySelector('.text');
-const fx = new TextScramble(el);
-
-let counter = 0;
-const next = () => {
-  fx.setText(phrases[counter]).then(() => {
-    setTimeout(next, 800);
-  });
-  counter = (counter + 1) % phrases.length;
-};
-
-next();
+ReactDOM.render(
+React.createElement(Clock, null),
+document.querySelector('.clock'));
